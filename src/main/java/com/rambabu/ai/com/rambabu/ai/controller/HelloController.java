4c810811f84c2509f4d3a2573bf4534cc9cda8a4
Rambabu Gonela <rambabu.gonela@gmail.com>
2026-07-08 11:34:47 +0530

    Initial Spring Boot Project

---
+++ src/main/java/com/rambabu/ai/com/rambabu/ai/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.rambabu.ai.com.rambabu.ai.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello Spring AI";
+    }
+}
---
+++ src/test/java/com/rambabu/ai/AiChatAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rambabu.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiChatAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
