06b904c9db3eb9c30b239185ba885a91675d7ffc
Rambabu Gonela <rambabu.gonela@gmail.com>
2026-07-08 13:49:04 +0530

    Integrate Spring AI with Google Gemini

--- src/main/java/com/rambabu/ai/com/rambabu/ai/service/ChatServiceImpl.java
+++ src/main/java/com/rambabu/ai/com/rambabu/ai/service/ChatServiceImpl.java
@@ -1,12 +1,21 @@
 package com.rambabu.ai.com.rambabu.ai.service;
 
 import com.rambabu.ai.com.rambabu.ai.dto.ChatResponse;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatServiceImpl implements ChatService{
+
+    final private ChatClient chatClient;
+
+    public ChatServiceImpl(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
     @Override
     public ChatResponse chat(String message) {
-        return new ChatResponse("Hello you asked -" +message);
+        String response = chatClient.prompt().user(message).call().content();
+        return new ChatResponse(response);
     }
 }
