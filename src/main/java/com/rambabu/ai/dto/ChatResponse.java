e68b723b8602e4071ee33be64da20f9b49373256
Rambabu Gonela <rambabu.gonela@gmail.com>
2026-07-08 16:00:30 +0530

    Enhance chat response with metadata

--- src/main/java/com/rambabu/ai/dto/ChatResponse.java
+++ src/main/java/com/rambabu/ai/dto/ChatResponse.java
@@ -2,8 +2,14 @@ package com.rambabu.ai.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
+import java.time.LocalDateTime;
+
 public record ChatResponse(
         @NotBlank(message = "Message")
-        String message
+        String response,
+        String model,
+        LocalDateTime timeStamp,
+        long processingTime
+
 ) {
 }
--- src/main/java/com/rambabu/ai/service/ChatServiceImpl.java
+++ src/main/java/com/rambabu/ai/service/ChatServiceImpl.java
@@ -2,20 +2,40 @@ package com.rambabu.ai.service;
 
 import com.rambabu.ai.dto.ChatResponse;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class ChatServiceImpl implements ChatService{
 
     final private ChatClient chatClient;
 
+    @Value("${spring.ai.google.genai.chat.options.model}")
+    private String modelName;
     public ChatServiceImpl(ChatClient.Builder builder) {
         this.chatClient = builder.build();
     }
 
     @Override
     public ChatResponse chat(String message) {
-        String response = chatClient.prompt().user(message).call().content();
-        return new ChatResponse(response);
+        long start = System.currentTimeMillis();
+
+        String response = chatClient
+                .prompt()
+                .user(message)
+                .call()
+                .content();
+
+        long end = System.currentTimeMillis();
+
+
+        return new ChatResponse(
+                response,
+                modelName,
+                LocalDateTime.now(),
+                end - start
+        );
     }
 }
